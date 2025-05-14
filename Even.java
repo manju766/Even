c3bde8014dd40321ae406244a8c800fbfe37a983
manju766 <mallikarjunmalli51946@gmail.com>
2025-05-14 12:28:05 +0530

    added

---
+++ Even.java
@@ -0,0 +1,15 @@
+public class Even {
+    public static void main(String[] args)
+    {
+        
+       System.out.println("Even numbers upto 10");
+       int num=10;
+        for(int i=1;i<=num;i++)
+        {
+          
+            System.out.println((i*2)+"");
+            
+        }
+    }
+    
+}
